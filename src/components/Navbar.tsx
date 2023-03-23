2d973ab4e78170356ddc657e95626d26706c6477
YARE0909 <pradyumnad092004@gmail.com>
2023-03-23 21:59:38 +0530

    💄 Updated mobile nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,27 +1,65 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 
 const Navbar = () => {
+  const [mobileNav, setMobileNav] = useState(false);
   return (
-    <div className="w-full h-[90px] flex justify-between items-center bg-black text-white font-Orbitron p-4">
-      <div>
-        <Link
-          href="/"
-          className="text-3xl md:text-4xl font-extrabold text-purple-600"
+    <div>
+      <div className="w-full h-[90px] flex justify-between items-center bg-black text-white font-Orbitron p-4">
+        <div>
+          <Link
+            href="/"
+            className="text-3xl md:text-4xl font-extrabold text-purple-600"
+          >
+            GDSC Tasks
+          </Link>
+        </div>
+
+        <div
+          className="p-4 text-purple-500 rounded-lg hover:text-white duration-300 font-bold md:hidden"
+          onClick={() => setMobileNav(!mobileNav)}
         >
-          GDSC Tasks
-        </Link>
+          <h1>Menu</h1>
+        </div>
+
+        <div className="hidden md:flex">
+          <Link
+            className="p-4 text-purple-500 rounded-lg hover:text-white duration-300 font-bold"
+            href="/weather"
+          >
+            Weather App
+          </Link>
+          <Link
+            className="p-4 text-purple-500 rounded-lg hover:text-white duration-300 font-bold"
+            href="/random"
+          >
+            Random text generator
+          </Link>
+          <Link
+            className="p-4 text-purple-500 rounded-lg hover:text-white duration-300 font-bold"
+            href="https://github.com/YARE0909/GDSC-Application"
+            target="_blank"
+          >
+            Repo
+          </Link>
+        </div>
       </div>
-      <div className="flex">
+      <div
+        className={
+          mobileNav ? "md:hidden w-full h-fit flex flex-col font-Orbitron border-t-2 border-b-2 border-gray-700 bg-black" : "hidden"
+        }
+      >
         <Link
           className="p-4 text-purple-500 rounded-lg hover:text-white duration-300 font-bold"
           href="/weather"
+          onClick={() => setMobileNav(!mobileNav)}
         >
           Weather App
         </Link>
         <Link
           className="p-4 text-purple-500 rounded-lg hover:text-white duration-300 font-bold"
           href="/random"
+          onClick={() => setMobileNav(!mobileNav)}
         >
           Random text generator
         </Link>
@@ -29,6 +67,7 @@ const Navbar = () => {
           className="p-4 text-purple-500 rounded-lg hover:text-white duration-300 font-bold"
           href="https://github.com/YARE0909/GDSC-Application"
           target="_blank"
+          onClick={() => setMobileNav(!mobileNav)}
         >
           Repo
         </Link>
